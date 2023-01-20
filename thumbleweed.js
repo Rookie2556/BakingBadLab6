565e4904c90bbbf9c5ed594f5759f21041b6c140
Thomas M <122787483+Rookie2556@users.noreply.github.com>
2023-01-20 10:14:50 +0000

    Create thumbleweed.js

---
+++ thumbleweed.js
@@ -0,0 +1,9 @@
+var tumbleweed = document.getElementById("tumbleweed");
+
+function startAnimation() {
+    tumbleweed.style.animation = "tumbleweed-animation 10s linear infinite";
+}
+
+function stopAnimation() {
+    tumbleweed.style.animation = "none";
+}
