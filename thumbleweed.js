96651d38ed38d9d20d7878fd176fafbf99846226
Thomas M <122787483+Rookie2556@users.noreply.github.com>
2023-01-20 11:49:42 +0000

    forgot you had to invoke, and added console log

--- thumbleweed.js
+++ thumbleweed.js
@@ -3,9 +3,12 @@ window.addEventListener("load", startAnimation);
 var tumbleweed = document.getElementById("tumbleweed");
 
 function startAnimation() {
+    console.log("Animation Started");
     tumbleweed.style.animation = "tumbleweed-animation 10s linear infinite";
 }
 
 function stopAnimation() {
     tumbleweed.style.animation = "none";
 }
+
+startAnimation()
