0fb40887f44860f6b874fb55a727f91639bb6e89
Thomas M <122787483+Rookie2556@users.noreply.github.com>
2023-01-20 02:27:06 +0000

    AddedFadeIn

--- RVFILE/IntroJs.js
+++ RVFILE/IntroJs.js
@@ -1,2 +1,10 @@
 let GrabAll = document.querySelectorAll(
     ":not(.RV):not(.LinksOnTop):not(.Introduction):not(.FootLinks)");
+
+setTimeout(function(){
+  let elements = document.querySelectorAll(".fade-in");
+  for(var i=0; i<elements.length; i++) {
+    elements[i].style.opacity = "1";
+  }
+}, 2000); // 2000 milliseconds = 2 seconds
+
