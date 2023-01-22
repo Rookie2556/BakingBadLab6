e232fc2dac71ef28777d4e2e89f068380e14bbfc
Thomas M <122787483+Rookie2556@users.noreply.github.com>
2023-01-22 22:39:29 +0000

    fadeinner was empty

--- RVFILE/Fader.js
+++ RVFILE/Fader.js
@@ -1,15 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+function fadeInner() {
     let elements = document.querySelectorAll(":not(.RV):not(.Introduction):not(.FootLinks)");
     for (let i = 0; i < elements.length; i++) {
       elements[i].classList.add("fade-in");
     }
-});
-
-//Add a function similar to above so I can repeat when needed.
-function fadeInner() {
 }
 
-//the above is unused and empty for the time being
-  
-fadeInner();
-
+document.addEventListener('DOMContentLoaded', function() {
+    fadeInner();
+});
