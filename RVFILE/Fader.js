73f54636b2aca78dfe52f5c59cd049216d3e6ae6
Thomas M <122787483+Rookie2556@users.noreply.github.com>
2023-01-22 22:27:31 +0000

    debug tip - dom loaded first so found code

--- RVFILE/Fader.js
+++ RVFILE/Fader.js
@@ -1,10 +1,11 @@
 window.onload = function() {
+  document.addEventListener('DOMContentLoaded', function() {
   let elements = //I used const, which outputted an error in chrome.
         document.querySelectorAll(":not(.RV):not(.Introduction):not(.FootLinks)");
   for (let i = 0; i < elements.length; i++) {
     elements[i].classList.add("fade-in");
   }
-}
+}});
 
 //Add a function similar to above so I can repeat when needed.
 function fadeInner() {
@@ -12,4 +13,5 @@ function fadeInner() {
 
 //the above is unused and empty for the time being
   
-
+fadeInner();
+  
