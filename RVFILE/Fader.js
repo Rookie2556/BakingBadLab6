602854c6c6fd706f6792a5f1055a202105e2aa27
Thomas M <122787483+Rookie2556@users.noreply.github.com>
2023-01-22 22:34:45 +0000

    both events unecessary

--- RVFILE/Fader.js
+++ RVFILE/Fader.js
@@ -1,11 +1,9 @@
-window.onload = function() {
-  document.addEventListener('DOMContentLoaded', function()) {
-  let elements = //I used const, which outputted an error in chrome.
-        document.querySelectorAll(":not(.RV):not(.Introduction):not(.FootLinks)");
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].classList.add("fade-in");
-  }
-}});
+document.addEventListener('DOMContentLoaded', function() {
+    let elements = document.querySelectorAll(":not(.RV):not(.Introduction):not(.FootLinks)");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.add("fade-in");
+    }
+});
 
 //Add a function similar to above so I can repeat when needed.
 function fadeInner() {
@@ -14,4 +12,4 @@ function fadeInner() {
 //the above is unused and empty for the time being
   
 fadeInner();
-  
+
